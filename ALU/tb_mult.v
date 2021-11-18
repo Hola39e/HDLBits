0d4fcbb097a1d5325be262f47edd638d6feefbca
Hola39e <e611hx@gmail.com>
2021-11-18 21:51:48 +0800

    Add pipeline mult

--- ALU/tb_mult.v
+++ ALU/tb_mult.v
@@ -85,4 +85,70 @@ module tb_mult ;
        $dumpvars(0, tb_mult);
    end
 
+       reg          data_rdy ;
+    reg [N-1:0]  mult1 ;
+    reg [M-1:0]  mult2 ;
+    wire                 res_rdy ;
+    wire [N+M-1:0]       res ;
+
+    //driver
+    initial begin
+        #55 ;
+        @(negedge clk ) ;
+        data_rdy  = 1'b1 ;
+        mult1  = 25;      mult2      = 5;
+        #10 ;      mult1  = 16;      mult2      = 10;
+        #10 ;      mult1  = 10;      mult2      = 4;
+        #10 ;      mult1  = 15;      mult2      = 7;
+        mult2      = 7;   repeat(32)    #10   mult1   = mult1 + 1 ;
+        mult2      = 1;   repeat(32)    #10   mult1   = mult1 + 1 ;
+        mult2      = 15;  repeat(32)    #10   mult1   = mult1 + 1 ;
+        mult2      = 3;   repeat(32)    #10   mult1   = mult1 + 1 ;
+        mult2      = 11;  repeat(32)    #10   mult1   = mult1 + 1 ;
+        mult2      = 4;   repeat(32)    #10   mult1   = mult1 + 1 ;
+        mult2      = 9;   repeat(32)    #10   mult1   = mult1 + 1 ;
+    end
+
+    //對輸入數據進行移位，方便後續校驗
+    reg  [N-1:0]   mult1_ref [M-1:0];
+    reg  [M-1:0]   mult2_ref [M-1:0];
+    always @(posedge clk) begin
+        mult1_ref[0] <= mult1 ;
+        mult2_ref[0] <= mult2 ;
+    end
+
+    genvar         i ;
+    generate
+        for(i=1; i<=M-1; i=i+1) begin
+            always @(posedge clk) begin
+            mult1_ref[i] <= mult1_ref[i-1];
+            mult2_ref[i] <= mult2_ref[i-1];
+            end
+        end
+    endgenerate
+   
+    //自校驗
+    reg  error_flag ;
+    always @(posedge clk) begin
+        # 1 ;
+        if (mult1_ref[M-1] * mult2_ref[M-1] != res && res_rdy) begin
+            error_flag <= 1'b1 ;
+        end
+        else begin
+            error_flag <= 1'b0 ;
+        end
+    end
+
+    //module instantiation
+    mult_man  #(.N(N), .M(M))
+        u_mult
+        (
+        .clk              (clk),
+        .rst_n             (rst_n),
+        .data_rdy         (data_rdy),
+        .mult1            (mult1),
+        .mult2            (mult2),
+        .res_rdy          (res_rdy),
+        .res              (res));
+
 endmodule // test
